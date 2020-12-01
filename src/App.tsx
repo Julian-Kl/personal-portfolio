c323e8c830954f71601aa1219bf38138bc3a8747
Julian-Kl <juliensworld@outlook.com>
2020-12-01 21:33:21 +0100

    Fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import './App.scss';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
     <React.Fragment>
       <div>
-        <h1>Hello World</h1>
-        <p>This is <a href="https://julianklummer.me">Julian-Kl</a>'s React Boilerplate</p>
-        <h2>Includes:</h2>
-        <ul>
-          <li>TypeScript</li>
-          <li>SASS</li>
-          <li>React Router</li>
-        </ul>
-        <h2>CSS Files</h2>
-        <ul>
-          <li>Normalize</li>
-        </ul>
+        <p>Hello I'm Julian</p>
       </div>
     </React.Fragment>
   );
 }
-
-export default App;
