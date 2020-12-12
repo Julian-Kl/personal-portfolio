f17e30bd3fe56cfbf477f09ccfb486b639d357df
Julian-Kl <juliensworld@outlook.com>
2020-12-12 16:15:01 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ const App: React.FC = () => {
             <Route path="/about">
               <ContactButton />
               <About />
-              <SkillCard />
             </Route>
             <Route path="/skills">
               <SkillCard />
