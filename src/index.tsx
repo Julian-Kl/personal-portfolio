6a4bbc4bfa7c689ef70005a1aaa286b0ea2d0d06
Julian Konsent <juliensworld@outlook.com>
2020-12-21 12:57:29 +0100

    Language Selector added
    Language Selector added

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,49 @@ import './normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<<<<<<< Updated upstream
+=======
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#80d6ff',
+      main: '#00B5FF',
+      dark: '#0077c2',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#b084f4',
+      main: '#7e56c1',
+      dark: '#4d2b90',
+      contrastText: '#000',
+    },
+  },
+});
+
+const LightTheme = React.lazy(() => import('./themes/lightTheme'));
+const DarkTheme = React.lazy(() => import('./themes/darkTheme'));
+
+const ThemeSelector: React.FC = ({ children }) => {
+  var chosenTheme = '';
+  if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+    chosenTheme = 'light';
+  } else {
+    chosenTheme = 'dark';
+  }
+
+  return (
+    <>
+      <React.Suspense fallback={<></>}>
+        {chosenTheme === 'light' && <LightTheme /> }
+        {chosenTheme === 'dark' && <DarkTheme /> }
+      </React.Suspense>
+      {children}
+    </>
+  )
+}
+>>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
